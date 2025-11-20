a3970db7650f39dfefa0848c1ddb63513a5e5e7b
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2025-11-20 10:18:48 +0100

    feat: commit inicial

---
+++ indexProyectoLoginLogoff.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Jesús Temprano Gallego</title>
+    <link rel="stylesheet" href="./webroot/css/style.css">
+</head>
+<body>
+    <!-- 😼 -->
+    <header>
+        <h1>CFGS - Desarrollo de Aplicaciones Web</h1>
+        <h3>Jesús Temprano Gallego</h3>
+        <p>Curso 2025/2026 - Grupo DAW2</p>
+    </header>
+    <!-- 😼 -->
+    <main>
+    </main>
+    <!-- 😼 -->
+    <footer>
+        <p><a href="../../" target="_self">Jesús Temprano Gallego</a> | 20/11/2025</p>
+        <p><a href="https://github.com/yatusabebeibe" target="_blank">Github</a><b>: </b><a href="https://github.com/yatusabebeibe/JTGDWESProyectoLoginLogoff/" target="_blank">Rama master</a><b> | </b><a href="https://github.com/yatusabebeibe/JTGDWESProyectoLoginLogoff/tree/developerJTG" target="_blank">Rama developer</a></p>
+    </footer>
+    <!-- 😼 -->
+    <!-- muxixima glasia alvelto pol el marivilliosiximo achetemeele que te paxo chatgepete -->
+</body>
+</html>
